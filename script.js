95febd756fe162b01bcae30edfae698221830d88
Devang2304 <devangvartak2304@gmail.com>
2022-08-25 03:04:30 +0530

    commit

--- script.js
+++ script.js
@@ -1,16 +1,72 @@
-let RockDiv=document.getElementById('Rock');
-let PaperDiv=document.getElementById('Paper');
-let ScissorDiv=document.getElementById('Scissors');
+const totalScore={computerScore:0,PlayerScore:0}
 
 function getComputerChoice() {
-    let string=['rock','paper','scissors'];
-    let randomNumber=(Math.floor(Math.random()*string.length));
-    // console.log(randomNumber);
-    // console.log(string[randomNumber]);
-    return string[randomNumber];
+    let rpsChoices = ['Rock', 'Paper', 'Scissors']
+  let computerChoice = rpsChoices[Math.floor(Math.random() * 3)]
+  return computerChoice
 }
+function getResult(playerChoice,computerChoice) {
+    let score;
+    if (playerChoice == computerChoice) {
+        score=0;
+    }
+    else if (playerChoice=='Rock' && computerChoice=='Scissors')   {
+        score=1;
+    }
+    else if (playerChoice=='Scissors' && computerChoice=='Paper')  {
+        score=1;
+    }
+    else if (playerChoice=='Paper' && computerChoice=='Rock'){
+        score =1;
+}else{
+    score=-1;
+}
+ return score;
+ }
+function showResult(score,playerChoice,computerChoice) {
+   const resultDiv=document.getElementById('result')
+   const handsDiv=document.getElementById('hands')
+   const playerScoreDiv=document.getElementById('player-score')
+   
+   if (score==-1) {
+    resultDiv.innerText='You lose!'
+   }else if(score ==0){
+    resultDiv.innerText="It's a tie!"
+   }else{
+    resultDiv.innerText='You win!'
+   }
+   handsDiv.innerText=`👱${playerChoice} vs 🤖${computerChoice}`
+   playerScoreDiv.innerText=totalScore['PlayerScore']
+   }
+
+
+ function onClickRPS(playerChoice) {
+    const computerChoice=getComputerChoice()
+    const score=getResult(playerChoice,computerChoice)
+    totalScore['PlayerScore']+=score
+    showResult(score,playerChoice,computerChoice)
+ }
+    
 function playGame() {
-    let Rock=document.querySelector('Rock');
-    let Paper=document.querySelector('Paper');
-    let Scissors=document.querySelector('Scissors');
+    let rpsButtons=document.querySelectorAll('.rpsButton')
+    //rpsButtons[0].onclick=()=>console.log(rpsButtons[0].value)
+
+    rpsButtons.forEach(rpsButton=>{
+        rpsButton.onclick=()=>onClickRPS(rpsButton.value)
+    })
+    const endGameButton=document.getElementById('endGameButton')
+    endGameButton.onclick=()=>endGame()
 }
+function endGame() {
+   totalScore['playerScore'] =0;
+   totalScore['computerChoice'] =0;
+   const resultDiv=document.getElementById('result')
+   const handsDiv=document.getElementById('hands')
+   const playerScoreDiv=document.getElementById('player-score')
+   resultDiv.innerText=''
+   handsDiv.innerText=''
+   playerScoreDiv.innerText=''
+}
+playGame()
+
+
